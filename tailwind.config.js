450f5bd1319184960fe86eb6cad384d11b302fba
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2024-08-17 07:29:14 +0300

    Stepper

--- tailwind.config.js
+++ tailwind.config.js
@@ -41,7 +41,7 @@ module.exports = {
         white: "#FFFFFF",
         black: "#121723",
         dark: "#1D2430",
-        primary: "#4A6CF7",
+        primary: "#14b8a6",
         yellow: "#FBB040",
         "bg-color-dark": "#171C28",
         "body-color": {
