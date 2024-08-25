7d762874425a62cd5cb3280dba19a0e9f93c18c7
Eng.Mahmood Anaam <eng.mahmood.anaam@gmail.com>
2024-08-26 00:26:45 +0300

    Update questions.js

--- src/components/Diagnostic/questions.js
+++ src/components/Diagnostic/questions.js
@@ -3,6 +3,7 @@ const questions = [
     question: "Do you often feel sad or depressed?",
     description: "This question aims to understand your general emotional state over a period of time.",
     answers: [
+      "No",
       "Rarely",
       "Sometimes",
       "Often",
@@ -12,6 +13,7 @@ const questions = [
     question: "Do you find it difficult to enjoy activities you used to enjoy?",
     description: "This question helps assess changes in your interest or pleasure in activities.",
     answers: [
+      "No",
       "Rarely",
       "Sometimes",
       "Often",
@@ -21,6 +23,7 @@ const questions = [
     question: "Do you often feel tired or low in energy most days?",
     description: "This question looks at your energy levels and how fatigue might affect your daily life.",
     answers: [
+      "No",
       "Rarely",
       "Sometimes",
       "Often",
@@ -30,6 +33,7 @@ const questions = [
     question: "Do you have frequent thoughts about death or suicide?",
     description: "This question is crucial in understanding the severity of your depressive symptoms.",
     answers: [
+      "No",
       "Rarely",
       "Sometimes",
       "Often",
@@ -39,6 +43,7 @@ const questions = [
     question: "Do you find it difficult to carry out simple daily tasks?",
     description: "This question evaluates how your depression might be impacting your daily functionality.",
     answers: [
+      "No",
       "Rarely",
       "Sometimes",
       "Often",
